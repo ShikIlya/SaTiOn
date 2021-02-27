418db7657921fe87d962ec8d215ca2bf5dd800f6
Nezar <hamideh@yandex.ru>
2021-02-27 19:38:41 +0300

    new registry btn color, location back

--- frontend/sation/src/app/authentification/registration/registration.component.ts
+++ frontend/sation/src/app/authentification/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-registration',
@@ -8,11 +9,14 @@ import { Router } from '@angular/router';
 })
 export class RegistrationComponent implements OnInit {
   hide = true;
-  constructor(private router: Router) { }
+  constructor(private router: Router, private location: Location) { }
 
   ngOnInit(): void {
   }
   onSubmit() {
 
+  }
+  goBack() {
+    this.location.back();
   }
 }
