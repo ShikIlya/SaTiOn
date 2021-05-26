e6d132851dbe2ebb735f10f963d3f1a1ee31741d
Marcusbille <misha10dmitrov@mail.ru>
2021-05-26 21:03:07 +0300

    Фикс timestamp

--- backend/sation-back/src/chat/models/message.entity.ts
+++ backend/sation-back/src/chat/models/message.entity.ts
@@ -1,29 +1,33 @@
-import { UserEntity } from "src/user/models/user.entity";
-import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
-import { ChatEntity } from "./chat.entity";
-
+import { UserEntity } from 'src/user/models/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { ChatEntity } from './chat.entity';
 
 @Entity()
 export class MessageEntity {
-
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column({ length: 500, nullable: false })
   content: string;
 
-  @ManyToOne(type => ChatEntity, chat => chat.messages, { nullable: false })
-  @JoinColumn({ name: "chatId", referencedColumnName: "id" })
-  chatId: string
+  @ManyToOne((type) => ChatEntity, (chat) => chat.messages, { nullable: false })
+  @JoinColumn({ name: 'chatId', referencedColumnName: 'id' })
+  chatId: string;
 
-  @ManyToOne(type => UserEntity, user => user.messages, { nullable: false })
-  @JoinColumn({ name: "senderId", referencedColumnName: "id" })
+  @ManyToOne((type) => UserEntity, (user) => user.messages, { nullable: false })
+  @JoinColumn({ name: 'senderId', referencedColumnName: 'id' })
   user: number;
 
   @Column()
   senderId: number;
 
-  @CreateDateColumn()
+  @CreateDateColumn({ type: 'timestamp with time zone' })
   creationTime: string;
-
 }
